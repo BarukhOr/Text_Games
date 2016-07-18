e8e3bbc7505ee2ac79d78b1eb67590bfb4334382
UtibeAbasi <1.BarukhOr@gmail.com>
2016-07-18 00:20:00 -0400

    Introduction to Constant Expressions
    + 2.14

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main() {
+	constexpr int WORD_LENGTH = 5;
+
 	cout << "Greetings and Welcome to Bulls and Cows" << endl;
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram that I am thinking of?" << endl;
 	return 0;
 }
