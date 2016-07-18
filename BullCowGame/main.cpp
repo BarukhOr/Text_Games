de41855800a6e5d2cdc97502a05e0169c2b7e4ed
UtibeAbasi <1.BarukhOr@gmail.com>
2016-07-18 00:20:25 -0400

    Simple i/o
    + 2.15
    + Major Bug: If user enters two words, the program does not print out the entire string. 
    + Rather it prints the first portion and then stops (Note: Given current implementation this is to be expected (this is how cout works)  however that is not the desired ... response)

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main() {
 	constexpr int WORD_LENGTH = 5;
-
-	cout << "Greetings and Welcome to Bulls and Cows" << endl;
+	
+	//Introduce the game
+	cout << "Greetings and Welcome to Bulls and Cows." << endl;
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram that I am thinking of?" << endl;
+
+	// Get a guess from the player
+	string Guess = "";
+	cout << "Enter your guess: ";
+	cin >> Guess;
+	cout << endl;
+
+	// Repeat the guess back to the user
+	cout << "The inputted word was: " << Guess << endl;
 	return 0;
 }
