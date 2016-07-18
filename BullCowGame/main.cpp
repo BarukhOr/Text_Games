9895297f92136171ee5a5afabb2f1dc34fbd41ac
UtibeAbasi <1.BarukhOr@gmail.com>
2016-07-18 01:14:11 -0400

    Clarity is worth fighting for
    + Added additional function to increase 'clarity'
    + Section 2.19

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -9,20 +9,14 @@ constexpr int WORD_LENGTH = 5;
 // Function declarations
 void PrintIntro();
 string GetGuess();
+void PlayGame();
 
 // The Entry point for this application
 int main() {
 	
 	PrintIntro();
-
-	string Guess = "";
-	constexpr int NUMBER_OF_TURNS = 5;
-	// Loop for the number of turns asking for guesses
-	for (int counter = 0; counter < NUMBER_OF_TURNS; counter++) {
-		Guess = GetGuess();
-		// Repeat the guess back to the user
-		cout << "The inputted word was: " << Guess << endl;
-	}
+	PlayGame();
+	return 0;	// Exit the application
 }
 
 // Introduce the game
@@ -38,4 +32,13 @@ string GetGuess() {
 	getline(cin, Guess);
 	cout << endl;
 	return Guess;
+}
+
+void PlayGame() {
+	constexpr int NUMBER_OF_TURNS = 5;
+	// Loop for the number of turns asking for guesses
+	for (int counter = 0; counter < NUMBER_OF_TURNS; counter++) {
+		string Guess = GetGuess();
+		cout << "The inputted word was: " << Guess << endl;
+	}
 }
